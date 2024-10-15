00c915a2c337c2b425d5bc81272583e12399facd
tacowasabii <godseoril812@gmail.com>
2024-10-16 02:37:25 +0900

    feat: input 컴포넌트 에러 스타일 추가

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,45 +1,57 @@
-import { VisibilityOffIcon, VisibilityOnIcon } from "@assets/svg";
+import { ErrorIcon, VisibilityOffIcon, VisibilityOnIcon } from "@assets/svg";
 import { forwardRef, InputHTMLAttributes, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  isInvalid?: boolean;
   className?: string;
 }
 
 const style =
-  "h-14 rounded-[4px] bg-surface-primary w-full px-4 text-md font-regular placeholder:text-disable focus:outline focus:outline-1 focus:outline-primary-500";
+  "h-14 rounded-[4px] bg-surface-primary w-full px-4 text-md font-regular placeholder:text-disable focus:outline focus:outline-1";
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, isInvalid, ...props }, ref) => {
     const [showPassword, setShowPassword] = useState<boolean>(false);
 
-    return type == "password" ? (
+    const inputStyle = twMerge(
+      style,
+      isInvalid
+        ? "outline outline-1 outline-red-500"
+        : "focus:outline-primary-500",
+      type === "password" ? "pr-14" : "",
+    );
+
+    const renderPasswordToggleIcon = () =>
+      showPassword ? (
+        <VisibilityOnIcon
+          onClick={() => setShowPassword((prev) => !prev)}
+          className="absolute right-6 top-1/2 -translate-y-1/2 transform cursor-pointer"
+        />
+      ) : (
+        <VisibilityOffIcon
+          onClick={() => setShowPassword((prev) => !prev)}
+          className="absolute right-6 top-1/2 -translate-y-1/2 transform cursor-pointer"
+        />
+      );
+
+    return (
       <div className={twMerge("relative", className)}>
         <input
           ref={ref}
-          type={showPassword ? "text" : "password"}
-          className={twMerge(style, "pr-14")}
+          type={
+            type === "password" ? (showPassword ? "text" : "password") : type
+          }
+          className={inputStyle}
           {...props}
         />
-        {showPassword ? (
-          <VisibilityOnIcon
-            onClick={() => setShowPassword((prev) => !prev)}
-            className="absolute right-6 top-1/2 -translate-y-1/2 transform cursor-pointer"
-          />
-        ) : (
-          <VisibilityOffIcon
-            onClick={() => setShowPassword((prev) => !prev)}
-            className="absolute right-6 top-1/2 -translate-y-1/2 transform cursor-pointer"
+        {type === "password" && renderPasswordToggleIcon()}
+        {isInvalid && (
+          <ErrorIcon
+            className={`absolute ${type === "password" ? "right-14" : "right-6"} top-1/2 -translate-y-1/2 transform`}
           />
         )}
       </div>
-    ) : (
-      <input
-        ref={ref}
-        type="text"
-        className={twMerge(style, className)}
-        {...props}
-      />
     );
   },
 );

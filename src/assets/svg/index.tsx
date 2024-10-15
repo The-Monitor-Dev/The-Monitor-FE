dba557bf408c1197dd13a3abbc4954b2067f19b6
tacowasabii <godseoril812@gmail.com>
2024-10-16 02:36:39 +0900

    feat: svg

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -4,3 +4,4 @@ export { default as VisibilityOnIcon } from "./visibilityOn.svg?react";
 export { default as CheckboxBlankIcon } from "./checkboxBlank.svg?react";
 export { default as CheckboxFillIcon } from "./checkboxFill.svg?react";
 export { default as CheckboxErrorIcon } from "./checkboxError.svg?react";
+export { default as ErrorIcon } from "./error.svg?react";

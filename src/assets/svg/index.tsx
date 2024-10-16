477bb0bf62d71a6aac14b4d2542b7d751b95ef64
Sohyunnnn <thgus05061@naver.com>
2024-10-16 22:47:03 +0900

    asset: add svg

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -5,3 +5,4 @@ export { default as CheckboxBlankIcon } from "./checkboxBlank.svg?react";
 export { default as CheckboxFillIcon } from "./checkboxFill.svg?react";
 export { default as CheckboxErrorIcon } from "./checkboxError.svg?react";
 export { default as ErrorIcon } from "./error.svg?react";
+export { default as SearchIcon } from "./search.svg?react";

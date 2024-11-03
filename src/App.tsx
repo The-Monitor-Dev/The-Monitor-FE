38aa959631f43d8bb20e6497f780b194491d7e6a
tacowasabii <godseoril812@gmail.com>
2024-11-03 15:47:17 +0900

    feat: 사이드메뉴 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "@components/Header";
 import { ChakraProvider } from "@chakra-ui/react";
 import SideMenu from "@components/SideMenu";
+import routes from "@constants/routes";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,14 +16,29 @@ const queryClient = new QueryClient({
 });
 
 function App() {
+  const location = useLocation();
+
+  const paths = [
+    routes.main,
+    routes.signIn,
+    routes.signUp,
+    routes.password,
+    routes.dashboard,
+  ];
+  const isPathInPaths = paths.includes(
+    location.pathname as (typeof paths)[number],
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
-        <div className="flex flex-col font-pretendard">
+        <div className="flex min-w-[1440px] flex-col font-pretendard">
           <Header />
           <div className="flex h-[calc(100vh-68px)] min-h-[794px] bg-base-bg">
-            <SideMenu />
-            <Outlet />
+            {!isPathInPaths && <SideMenu />}
+            <div className="flex-grow">
+              <Outlet />
+            </div>
           </div>
         </div>
       </ChakraProvider>

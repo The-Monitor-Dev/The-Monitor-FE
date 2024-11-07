c4b3271eb4ac2ae559bec548e680ceea6efda918
Sohyunnnn <thgus05061@naver.com>
2024-11-07 22:07:39 +0900

    style: 스타일 수정

--- src/pages/Dashboard/components/DeleteModal/index.tsx
+++ src/pages/Dashboard/components/DeleteModal/index.tsx
@@ -33,14 +33,14 @@ const DeleteModal: React.FC<DeleteModalProps> = ({ onClose }) => {
         <div className="mt-6 flex justify-center gap-3">
           <button
             type="button"
-            className="w-[111px] rounded bg-error-500 py-2 text-white"
+            className="w-[111px] rounded bg-error-500 py-2 text-lg font-semibold text-white"
             onClick={handleModalClose}
           >
             삭제하기
           </button>
           <button
             type="button"
-            className="w-[111px] rounded border border-error-500 py-2 text-error-500"
+            className="w-[111px] rounded border border-error-500 py-2 text-lg font-semibold text-error-500"
             onClick={handleModalClose}
           >
             취소하기

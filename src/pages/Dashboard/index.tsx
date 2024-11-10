245bb8c996d3c494e21f4f05f5ef7e6ce0cd6377
Sohyunnnn <108279922+Sohyunnnn@users.noreply.github.com>
2024-11-10 23:27:28 +0900

    [style/#36] 대시보드 페이지 스타일 수정 (#37)
    ## 💻 관련 이슈
    
    <!--
    ex) close #100
    -->
    
    - close #36 
    
    ## 💡 작업내용
    
    - 대시보드 페이지 메인 디자인 수정
    
    ## 🧐 참고 사항
    
    - 컴포넌트화는 나중에 할 예정입니다.
    
    ## 🖼️ 스크린샷
    
    <img width="1470" alt="image"
    src="https://github.com/user-attachments/assets/6a83012c-2e34-4244-9738-7e14a78e7e79">
    
    
    ## ️✅ 체크리스트 (PR 올리기 전 아래 내용을 확인해 주세요)
    
    - [x] Reviewers를 지정해주세요
    - [x] Assignees는 본인을 선택해주세요
    - [x] label을 선택해주세요

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -59,10 +59,6 @@ const DashboardPage: React.FC = () => {
     setIsAddModalOpen(true);
   };
 
-  const handleAddModalClose = () => {
-    setIsAddModalOpen(false);
-  };
-
   const handleEidtModalOpen = () => {
     setIsEditModalOpen(true);
   };
@@ -86,8 +82,8 @@ const DashboardPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="mt-[63px] flex w-[1048px] justify-between">
+    <div className="flex flex-col items-center">
+      <div className="mb-4 mt-[63px] flex w-[1048px] justify-between">
         <div className="flex h-11 w-[480px] items-center rounded border bg-white focus-within:border-primary-500">
           <input
             className="w-[436px] px-4 text-md font-regular placeholder:text-md placeholder:font-regular focus:outline-none"
@@ -97,77 +93,69 @@ const DashboardPage: React.FC = () => {
         </div>
         <button
           type="button"
-          className="flex h-10 items-center gap-1 rounded border-[0.5px] border-primary-200 bg-surface-secondary p-2 pl-3 text-md font-semibold text-primary-700"
+          className="flex h-10 gap-1 rounded border-[0.5px] border-primary-200 bg-surface-secondary p-2 pl-3 text-md font-semibold text-primary-700"
           onClick={handleAddModalOpen}
         >
           고객사 추가하기
           <AddCircleFillIcon className="fill-primary-500" />
         </button>
       </div>
-      <div className="group mt-4 flex h-[108px] w-[1048px] items-center rounded bg-white shadow-main">
-        <button
-          type="button"
-          className="flex w-full group-hover:bg-neutral-100"
-        >
-          <img src={DefaultImage} className="m-4 w-24 rounded" />
-          <div className="flex w-full flex-col gap-1 py-5 pl-4 pr-5">
-            <h2 className="text-left text-2xl font-semibold text-title">
-              한솥
-            </h2>
-            <div className="flex items-center">
-              <PersonIcon className="mr-1" />
-              <p className="mr-2 text-md font-regular text-body3">담당자</p>
-              <p className="text-md font-semibold text-body1">이현수</p>
+      <div className="grid w-[1048px] grid-cols-4 gap-3">
+        <div className="flex w-[253px] rounded bg-white">
+          <div className="flex w-full flex-col">
+            <div className="relative flex justify-end px-3 py-2">
+              <button
+                type="button"
+                className={`rounded-sm p-[6px] ${isMenuOpen ? "bg-neutral-100" : "hover:bg-neutral-100"}`}
+                onClick={handleMenuToggle}
+              >
+                <MoreHorizIcon />
+              </button>
+
+              {isMenuOpen && (
+                <div
+                  ref={menuRef}
+                  className="absolute left-[204px] top-[52px] rounded border border-neutral-200 bg-white"
+                >
+                  <button
+                    type="button"
+                    className="flex w-32 px-5 py-2 text-md font-medium hover:bg-neutral-100"
+                    onClick={() => {
+                      handleEidtModalOpen();
+                      handleMenuClose();
+                    }}
+                  >
+                    수정하기
+                    <EditSquareIcon className="ml-2" />
+                  </button>
+                  <div className="border-neutral-200" />
+                  <button
+                    type="button"
+                    className="flex w-32 px-5 py-2 text-md font-medium hover:bg-neutral-100"
+                    onClick={() => {
+                      handleDeleteModalOpen();
+                      handleMenuClose();
+                    }}
+                  >
+                    삭제하기
+                    <DeleteIcon className="ml-2" />
+                  </button>
+                </div>
+              )}
+            </div>
+            <div className="flex justify-center border-b-1 border-neutral-200">
+              <img src={DefaultImage} className="mb-10 mt-5 w-24 rounded" />
+            </div>
+            <div className="flex w-full flex-col gap-1 py-4 pl-4 pr-5">
+              <h2 className="text-left text-xl font-semibold text-title">
+                한솥
+              </h2>
+              <div className="flex items-center">
+                <PersonIcon className="mr-[6px]" />
+                <p className="text-md font-semibold text-disable">이현수</p>
+              </div>
             </div>
           </div>
-        </button>
-        <div
-          className="relative flex h-full w-[92px] cursor-default items-center border-l-[0.5px] border-neutral-200 px-7 group-hover:bg-neutral-100"
-          onMouseEnter={(e) =>
-            e.currentTarget.parentElement?.classList.remove("group")
-          }
-          onMouseLeave={(e) =>
-            e.currentTarget.parentElement?.classList.add("group")
-          }
-        >
-          <button
-            type="button"
-            className={`rounded-[2.25px] p-[6px] ${isMenuOpen ? "bg-neutral-100" : "hover:bg-neutral-100"}`}
-            onClick={handleMenuToggle}
-          >
-            <MoreHorizIcon />
-          </button>
-
-          {isMenuOpen && (
-            <div
-              ref={menuRef}
-              className="absolute left-7 top-[76px] rounded border border-neutral-200 bg-white"
-            >
-              <button
-                type="button"
-                className="flex w-32 px-5 py-2 text-md font-medium hover:bg-neutral-100"
-                onClick={() => {
-                  handleEidtModalOpen();
-                  handleMenuClose();
-                }}
-              >
-                수정하기
-                <EditSquareIcon className="ml-2" />
-              </button>
-              <div className="border-t border-neutral-200" />
-              <button
-                type="button"
-                className="flex w-32 px-5 py-2 text-md font-medium hover:bg-neutral-100"
-                onClick={() => {
-                  handleDeleteModalOpen();
-                  handleMenuClose();
-                }}
-              >
-                삭제하기
-                <DeleteIcon className="ml-2" />
-              </button>
-            </div>
-          )}
         </div>
       </div>
       {isDeleteModalOpen && <DeleteModal onClose={handleDeleteModalClose} />}

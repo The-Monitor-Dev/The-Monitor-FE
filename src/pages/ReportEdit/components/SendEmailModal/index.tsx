9537120ef601b1eecbed7bb2cfecd2ecb788455d
Juhyeon Lee <godseoril812@gmail.com>
2024-11-17 19:48:12 +0900

    [Feature/#47] 메일 전송 모달 (#48)
    ## 💻 관련 이슈
    
    <!--
    ex) close #100
    -->
    
    - close #47
    
    ## 💡 작업내용
    
    - 메일 전송하기 모달
    
    ## 🧐 참고 사항
    
    -
    
    ## 🖼️ 스크린샷
    
    
    https://github.com/user-attachments/assets/eb926e8d-477a-45a1-adc8-c19551824393
    
    
    ## ️✅ 체크리스트 (PR 올리기 전 아래 내용을 확인해 주세요)
    
    - [x] Reviewers를 지정해주세요
    - [x] Assignees는 본인을 선택해주세요
    - [x] label을 선택해주세요

---
+++ src/pages/ReportEdit/components/SendEmailModal/index.tsx
@@ -0,0 +1,101 @@
+import { CloseIcon } from "@assets/svgs";
+import Button from "@components/Button";
+import Input from "@components/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface SendEmailModalProps {
+  onClose: () => void;
+}
+
+type SendEmailFormData = {
+  title: string;
+  body: string;
+};
+
+const SendEmailModal: React.FC<SendEmailModalProps> = ({ onClose }) => {
+  const { register, handleSubmit, watch } = useForm<SendEmailFormData>();
+  const [title, body] = watch(["title", "body"]);
+
+  const onSubmit: SubmitHandler<SendEmailFormData> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-65">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="h-[665px] w-[580px] bg-white"
+      >
+        <div className="mr-5 mt-5 flex justify-end">
+          <button
+            onClick={onClose}
+            className="flex h-8 w-8 items-center justify-center"
+          >
+            <CloseIcon className="h-7 w-7 fill-neutral-700" />
+          </button>
+        </div>
+        <div className="mt-3 flex flex-col px-[50px]">
+          <span className="text-4xl font-semibold text-title">
+            메일 전송하기
+          </span>
+          <div className="mt-2 text-title underline">메일 예시 보기</div>
+          <div className="mt-4 flex flex-col bg-surface-disable px-3 py-4 text-body3">
+            <span>
+              &nbsp;&bull;&nbsp; 미리 설정된 수신인에게 메일이 발송됩니다.
+            </span>
+            <span>
+              &nbsp;&bull;&nbsp; 파일 미리보기와 엑셀 파일이 포함되어
+              전송됩니다.
+            </span>
+          </div>
+          <div className="mt-4 flex flex-col gap-6 px-1">
+            <div className="flex flex-col gap-2">
+              <label className="flex items-end gap-1">
+                <span className="text-md font-semibold text-title">
+                  메일 제목
+                </span>
+                <span className="text-sm font-medium text-body3">(필수)</span>
+              </label>
+              <Input
+                type="text"
+                placeholder="메일 제목을 입력해주세요."
+                {...register("title", { required: true })}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label className="flex items-end gap-1">
+                <span className="text-md font-semibold text-title">
+                  메일 본문
+                </span>
+                <span className="text-sm font-medium text-body3">(선택)</span>
+              </label>
+              <div className="flex flex-col gap-2 rounded border-1 border-transparent bg-surface-primary px-4 pb-2 pt-3 focus-within:border-primary-500">
+                <textarea
+                  {...register("body")}
+                  maxLength={500}
+                  placeholder="본문 내용을 입력해주세요."
+                  className="h-[96px] resize-none bg-surface-primary text-md font-regular outline-none placeholder:text-disable"
+                />
+                <div className="self-end text-xs font-medium text-body3">
+                  {body?.length}/500
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-[39px] flex w-full justify-center">
+          <Button
+            type="submit"
+            style="filled"
+            disabled={!title}
+            className="w-[360px] py-3 text-md font-semibold"
+          >
+            전송하기
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SendEmailModal;

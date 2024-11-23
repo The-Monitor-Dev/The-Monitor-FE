85caffc023aadfc1df603cd63c50b05de693b127
Sohyunnnn <thgus05061@naver.com>
2024-11-23 16:11:27 +0900

    style: 스타일 수정

--- src/pages/Dashboard/components/KeywordLabel/index.tsx
+++ src/pages/Dashboard/components/KeywordLabel/index.tsx
@@ -7,14 +7,12 @@ interface KeywordLabelProps {
 
 const KeywordLabel: React.FC<KeywordLabelProps> = ({ label, isRequired }) => {
   return (
-    <div className="flex flex-col">
-      <label className="flex items-center">
-        <p className="mr-[6px] text-md font-semibold text-title">{label}</p>
-        <p className="text-sm font-regular text-body3">
-          [{isRequired ? "필수" : "선택"}]
-        </p>
-      </label>
-    </div>
+    <label className="mb-4 flex items-center">
+      <p className="mr-[6px] text-md font-semibold text-title">{label}</p>
+      <p className="text-sm font-regular text-body3">
+        [{isRequired ? "필수" : "선택"}]
+      </p>
+    </label>
   );
 };
 

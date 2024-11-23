e262f291c6a82ce1af7cfb9ac55855b55f559472
Sohyunnnn <thgus05061@naver.com>
2024-11-23 16:21:16 +0900

    refactor: 컴포넌트 분리

---
+++ src/pages/Setting/KeywordList/index.tsx
@@ -0,0 +1,44 @@
+import KeywordInput from "@components/KeywordInput";
+
+interface KeywordListProps {
+  title: string;
+  keywords: string[];
+  onAddKeyword: (keyword: string) => void;
+  onDeleteKeyword: (keyword: string) => void;
+  type: string;
+}
+
+const KeywordList = ({
+  title,
+  keywords,
+  onAddKeyword,
+  onDeleteKeyword,
+  type,
+}: KeywordListProps) => {
+  return (
+    <div className="w-[340px] border-r-1 border-neutral-200">
+      <div className="border-b-1 border-neutral-200 px-5 py-4 text-xl font-medium">
+        {title}
+      </div>
+      <div className="my-5 max-h-[600px] overflow-y-auto px-6">
+        <KeywordInput
+          placeholder={
+            type === "email"
+              ? "메일 주소 입력 후 엔터(Enter)를 눌러주세요."
+              : "키워드 입력 후 엔터(Enter)를 눌러주세요."
+          }
+          keywords={keywords}
+          onAddKeyword={onAddKeyword}
+          onDeleteKeyword={onDeleteKeyword}
+          duplicateErrorMessage={
+            type === "email"
+              ? "*이미 추가된 이메일입니다."
+              : "*이미 추가된 키워드입니다."
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+export default KeywordList;
--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -1,15 +1,28 @@
 import Button from "@components/Button";
-import KeywordInput from "@components/KeywordInput";
 import { useState } from "react";
+import KeywordList from "./KeywordList";
 
 const SettingPage = () => {
   const [selfKeywords, setSelfKeywords] = useState<string[]>([]);
   const [competitorKeywords, setCompetitorKeywords] = useState<string[]>([]);
   const [industryKeywords, setIndustryKeywords] = useState<string[]>([]);
+  const [recipientEmails, setRecipientEmails] = useState<string[]>([]);
+  const [ccEmails, setCcEmails] = useState<string[]>([]);
   const [activeTab, setActiveTab] = useState("검색 키워드");
 
+  const updateFunctions: Record<
+    "SELF" | "COMPETITOR" | "INDUSTRY" | "RECIPIENT" | "CC",
+    React.Dispatch<React.SetStateAction<string[]>>
+  > = {
+    SELF: setSelfKeywords,
+    COMPETITOR: setCompetitorKeywords,
+    INDUSTRY: setIndustryKeywords,
+    RECIPIENT: setRecipientEmails,
+    CC: setCcEmails,
+  };
+
   const handleKeywordChange = (
-    category: "SELF" | "COMPETITOR" | "INDUSTRY",
+    category: "SELF" | "COMPETITOR" | "INDUSTRY" | "RECIPIENT" | "CC",
     keyword: string,
     action: "add" | "delete",
   ) => {
@@ -18,13 +31,7 @@ const SettingPage = () => {
         ? [...new Set([...keywords, keyword])]
         : keywords.filter((k) => k !== keyword);
 
-    if (category === "SELF") {
-      setSelfKeywords((prev) => updateKeywords(prev));
-    } else if (category === "COMPETITOR") {
-      setCompetitorKeywords((prev) => updateKeywords(prev));
-    } else if (category === "INDUSTRY") {
-      setIndustryKeywords((prev) => updateKeywords(prev));
-    }
+    updateFunctions[category]((prev) => updateKeywords(prev));
   };
 
   return (
@@ -55,13 +62,11 @@ const SettingPage = () => {
         </Button>
       </div>
       <div className="ml-8 flex h-[662px] w-full">
-        <div className="w-[340px] border-r-1 border-neutral-200">
-          <div className="border-b-1 border-neutral-200 px-5 py-4 text-xl font-medium">
-            자사
-          </div>
-          <div className="my-5 max-h-[600px] overflow-y-auto px-6">
-            <KeywordInput
-              placeholder="키워드 입력 후 엔터(Enter)를 눌러주세요."
+        {activeTab === "검색 키워드" ? (
+          <>
+            <KeywordList
+              title="자사"
+              type="keyword"
               keywords={selfKeywords}
               onAddKeyword={(keyword) =>
                 handleKeywordChange("SELF", keyword, "add")
@@ -69,17 +74,10 @@ const SettingPage = () => {
               onDeleteKeyword={(keyword) =>
                 handleKeywordChange("SELF", keyword, "delete")
               }
-              duplicateErrorMessage="*이미 추가된 키워드입니다."
             />
-          </div>
-        </div>
-        <div className="w-[340px] border-r-1 border-neutral-200">
-          <div className="border-b-1 border-neutral-200 px-5 py-4 text-xl font-medium">
-            경쟁사
-          </div>
-          <div className="my-5 max-h-[600px] overflow-y-auto px-6">
-            <KeywordInput
-              placeholder="키워드 입력 후 엔터(Enter)를 눌러주세요."
+            <KeywordList
+              title="경쟁사"
+              type="keyword"
               keywords={competitorKeywords}
               onAddKeyword={(keyword) =>
                 handleKeywordChange("COMPETITOR", keyword, "add")
@@ -87,17 +85,10 @@ const SettingPage = () => {
               onDeleteKeyword={(keyword) =>
                 handleKeywordChange("COMPETITOR", keyword, "delete")
               }
-              duplicateErrorMessage="*이미 추가된 키워드입니다."
             />
-          </div>
-        </div>
-        <div className="h-full w-[340px]">
-          <div className="border-b-1 border-neutral-200 px-5 py-4 text-xl font-medium">
-            업계
-          </div>
-          <div className="my-5 max-h-[600px] overflow-y-auto px-6">
-            <KeywordInput
-              placeholder="키워드 입력 후 엔터(Enter)를 눌러주세요."
+            <KeywordList
+              title="업계"
+              type="keyword"
               keywords={industryKeywords}
               onAddKeyword={(keyword) =>
                 handleKeywordChange("INDUSTRY", keyword, "add")
@@ -105,10 +96,34 @@ const SettingPage = () => {
               onDeleteKeyword={(keyword) =>
                 handleKeywordChange("INDUSTRY", keyword, "delete")
               }
-              duplicateErrorMessage="*이미 추가된 키워드입니다."
             />
-          </div>
-        </div>
+          </>
+        ) : (
+          <>
+            <KeywordList
+              title="수신인"
+              type="email"
+              keywords={recipientEmails}
+              onAddKeyword={(keyword) =>
+                handleKeywordChange("RECIPIENT", keyword, "add")
+              }
+              onDeleteKeyword={(keyword) =>
+                handleKeywordChange("RECIPIENT", keyword, "delete")
+              }
+            />
+            <KeywordList
+              title="참조인"
+              type="email"
+              keywords={ccEmails}
+              onAddKeyword={(keyword) =>
+                handleKeywordChange("CC", keyword, "add")
+              }
+              onDeleteKeyword={(keyword) =>
+                handleKeywordChange("CC", keyword, "delete")
+              }
+            />
+          </>
+        )}
       </div>
     </div>
   );

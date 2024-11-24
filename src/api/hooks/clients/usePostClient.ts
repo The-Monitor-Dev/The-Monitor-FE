13278394a91164e3359f0b3c13a8f1d6d7c71d76
Sohyunnnn <thgus05061@naver.com>
2024-11-24 21:36:25 +0900

    feat: 낙관적 업데이트 추가

--- src/api/hooks/clients/usePostClient.ts
+++ src/api/hooks/clients/usePostClient.ts
@@ -1,10 +1,25 @@
-import { useMutation } from "@tanstack/react-query";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { postClient } from "@api/clientsAPI";
 
 const usePostClient = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (data: FormData) => {
       return postClient(data);
+    },
+    onMutate: async (data) => {
+      const previousClients = queryClient.getQueryData(["clients"]);
+
+      queryClient.setQueryData(["clients"], (oldData: any) => ({
+        ...oldData,
+        clients: [...(oldData?.clients || []), { ...data }],
+      }));
+
+      return { previousClients };
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["clients"] });
     },
   });
 };

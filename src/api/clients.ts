b5852cf031b6d8602348427b65db146259ffbb7f
Sohyunnnn <thgus05061@naver.com>
2024-11-24 15:23:54 +0900

    rename: 파일명 수정

--- src/api/clients.ts
+++
@@ -1,6 +0,0 @@
-import { authApiGet } from "./apiUtils";
-import { GetClientInfo } from "./types/clients";
-
-export const getClientInfo = async (clientId: number) => {
-  return authApiGet<GetClientInfo>("/clients/info", { clientId });
-};
--- src/api/clientsAPI.ts
+++ src/api/clientsAPI.ts
@@ -1,6 +1,10 @@
 import { authApiGet } from "./apiUtils";
-import { Client } from "./types/clients";
+import { Client, GetClientInfo } from "./types/clients";
 
 export const getClients = async () => {
   return authApiGet<Client[]>("/clients");
+};
+
+export const getClientInfo = async (clientId: number) => {
+  return authApiGet<GetClientInfo>("/clients/info", { clientId });
 };

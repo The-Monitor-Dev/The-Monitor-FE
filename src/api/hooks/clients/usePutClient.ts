2985db7f78147539cfdd8197afdcf22b1a5055cb
Sohyunnnn <thgus05061@naver.com>
2024-11-25 14:40:50 +0900

    fix: toast 문제 해결

--- src/api/hooks/clients/usePutClient.ts
+++ src/api/hooks/clients/usePutClient.ts
@@ -1,23 +1,18 @@
 import { putClient } from "@api/clientsAPI";
-import { Toast } from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const usePutClient = () => {
+  const toast = useToast();
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: ({ clientId, data }: { clientId: number; data: FormData }) =>
       putClient(clientId, data),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["clients"] });
-      Toast({
+      toast({
         title: "고객사 정보가 수정되었습니다.",
         status: "success",
-      });
-    },
-    onError: () => {
-      Toast({
-        title: "고객사 정보 수정 실패",
-        status: "error",
       });
     },
   });

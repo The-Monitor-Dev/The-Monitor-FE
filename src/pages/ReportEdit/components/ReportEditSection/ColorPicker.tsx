267bddd75482fc81e60a339463765ab7f767eb6b
tacowasabii <godseoril812@gmail.com>
2024-11-26 05:15:06 +0900

    feat: 보고서 편집 페이지

---
+++ src/pages/ReportEdit/components/ReportEditSection/ColorPicker.tsx
@@ -0,0 +1,94 @@
+import React, { KeyboardEvent, useRef, useState } from "react";
+import { HexColorPicker } from "react-colorful";
+import { useOutsideClick } from "@chakra-ui/react";
+import usePatchReportColor from "@api/hooks/reports/usePatchReportColor";
+import { clientId } from "@constants/clientId";
+
+interface ColorPickerProps {
+  reportId: number;
+  color: string;
+  onChangeColor: (color: string) => void;
+}
+
+const ColorPicker: React.FC<ColorPickerProps> = ({
+  reportId,
+  color,
+  onChangeColor,
+}) => {
+  const [isColorPickerOpen, setIsColorPickerOpen] = useState(false);
+  const [isColorEditing, setIsColorEditing] = useState(false);
+
+  const colorPickerRef = useRef<HTMLDivElement | null>(null);
+  const colorInputRef = useRef<HTMLInputElement>(null);
+  const { mutate: updateColor } = usePatchReportColor();
+
+  const handleMutateColor = () => {
+    updateColor({ clientId: clientId, reportId, data: { color } });
+  };
+
+  useOutsideClick({
+    ref: colorPickerRef,
+    handler: () => {
+      setIsColorPickerOpen(false);
+      handleMutateColor();
+    },
+  });
+
+  const handleColorClick = () => {
+    setIsColorEditing(true);
+    setTimeout(() => colorInputRef.current?.focus(), 0);
+  };
+
+  const handleColorBlur = () => {
+    if (/^#[0-9A-F]{6}$/i.test(color)) {
+      setIsColorEditing(false);
+      handleMutateColor();
+    } else {
+      alert("유효한 HEX 색상 코드를 입력하세요.");
+      onChangeColor("#FFFFFF");
+    }
+  };
+
+  const handleColorKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleColorBlur();
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-2 rounded-3xl border-1 border-neutral-200 bg-white px-3 py-2">
+      <div ref={colorPickerRef} className="relative h-7 w-7">
+        <button
+          type="button"
+          onClick={() => setIsColorPickerOpen((prev) => !prev)}
+          className="h-7 w-7 rounded-full border-1 border-[#D9D9D9]"
+          style={{ backgroundColor: color }}
+        />
+        {isColorPickerOpen && (
+          <div className="absolute right-0 top-14 z-10">
+            <HexColorPicker color={color} onChange={onChangeColor} />
+          </div>
+        )}
+      </div>
+      {isColorEditing ? (
+        <input
+          ref={colorInputRef}
+          value={color.toUpperCase()}
+          onChange={(e) => onChangeColor(e.target.value)}
+          onBlur={handleColorBlur}
+          onKeyDown={handleColorKeyDown}
+          className="w-[68px] text-sm font-semibold text-body1 outline-none"
+        />
+      ) : (
+        <span
+          className="w-[68px] cursor-pointer text-sm font-semibold text-body1"
+          onClick={handleColorClick}
+        >
+          {color?.toUpperCase()}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default ColorPicker;
--- src/pages/ReportEdit/index.tsx
+++ src/pages/ReportEdit/index.tsx
@@ -1,93 +1,19 @@
-import useGetReportDetails from "@api/hooks/reports/useGetReportDetails";
-import Button from "@components/Button";
-import Dropdown from "@components/Dropdown";
-import Input from "@components/Input";
 import ReportHeader from "@features/report/ReportHeader";
-import ReportSection from "@features/report/ReportSection";
-import { useState } from "react";
 import { useParams } from "react-router-dom";
+import ReportEditSection from "./components/ReportEditSection";
+import AddArticleForm from "./components/AddArticleForm";
 
 const ReportEditPage = () => {
   const { reportId } = useParams<{ reportId: string }>();
   const numericReportId = Number(reportId);
-
-  const categoryOptions = ["자사", "경쟁사", "업계"];
-  const [selectedOption, setSelectedOption] = useState("자사");
-  const handleSelectOption = (option: string) => {
-    setSelectedOption(option);
-  };
-  const { data: reportDetail } = useGetReportDetails({
-    clientId: 1,
-    reportId: numericReportId,
-  });
 
   return (
     <div className="flex h-full justify-center">
       <div className="flex w-[1280px] flex-col">
         <ReportHeader />
         <div className="mt-4 flex flex-grow gap-7">
-          <ReportSection
-            initialTitle={reportDetail?.title}
-            initialColor={reportDetail?.color}
-            reportId={numericReportId}
-          />
-
-          <div className="flex h-[667px] flex-col rounded bg-white">
-            <span className="border-b-1 border-neutral-200 px-6 py-5 text-xl font-semibold text-title">
-              기사 추가하기
-            </span>
-            <div className="flex flex-col gap-5 px-7 py-6">
-              <div className="flex items-center gap-5">
-                <div className="flex w-[108px] flex-col gap-2">
-                  <div className="text-sm font-medium text-title">카테고리</div>
-                  <Dropdown
-                    width={108}
-                    options={categoryOptions}
-                    selectedOption={selectedOption}
-                    onSelectOption={handleSelectOption}
-                  />
-                </div>
-                <div className="flex flex-grow flex-col gap-2">
-                  <div className="text-sm font-medium text-title">키워드</div>
-                  <Dropdown
-                    width={216}
-                    options={categoryOptions}
-                    selectedOption={selectedOption}
-                    onSelectOption={handleSelectOption}
-                  />
-                </div>
-              </div>
-              <div className="flex w-[344px] flex-col gap-3 text-sm font-medium">
-                <div className="flex flex-col gap-2">
-                  <label>헤드라인</label>
-                  <Input placeholder="기사제목을 입력해주세요." />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <label>URL</label>
-                  <Input placeholder="기사의 링크를 첨부해주세요." />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <label>발행일자</label>
-                  <Input placeholder="YYYY.MM.DD " />
-                </div>
-                <div className="flex items-center gap-5">
-                  <div className="flex flex-col gap-2">
-                    <label>미디어</label>
-                    <Input placeholder="예) 가나일보" />
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <label>기자</label>
-                    <Input placeholder="예) 홍길동" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex justify-center">
-              <Button type="submit" style="filled" className="w-[360px] py-3">
-                추가하기
-              </Button>
-            </div>
-          </div>
+          <ReportEditSection reportId={numericReportId} />
+          <AddArticleForm reportId={numericReportId} />
         </div>
       </div>
     </div>
